42ff245a189387d64ea5aa5e58dd074870d0b06b
frankrb27 <32852903+frankrb27@users.noreply.github.com>
2017-11-13 18:37:33 -0500

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,14 +1,9 @@
-chmod +x /AdministrarConvenios/start.sh
-chmod +x /AdministrarFactura/start.sh
-chmod +x /AdministrarCompensacion/start.sh
-chmod +x /SAPAutenticacion/start.sh
+chmod +x AdministrarConvenios/start.sh
+chmod +x AdministrarFactura/start.sh
+chmod +x AdministrarCompensacion/start.sh
+chmod +x SAPAutenticacion/start.sh
 
-chmod +x /AdministrarConvenios/stop.sh
-chmod +x /AdministrarFactura/stop.sh
-chmod +x /AdministrarCompensacion/stop.sh
-chmod +x /SAPAutenticacion/stop.sh
-
-./AdministrarConvenios/start.sh
-./AdministrarFactura/start.sh
-./AdministrarCompensacion/start.sh
-./SAPAutenticacion/start.sh
+chmod +x AdministrarConvenios/stop.sh
+chmod +x AdministrarFactura/stop.sh
+chmod +x AdministrarCompensacion/stop.sh
+chmod +x SAPAutenticacion/stop.sh
