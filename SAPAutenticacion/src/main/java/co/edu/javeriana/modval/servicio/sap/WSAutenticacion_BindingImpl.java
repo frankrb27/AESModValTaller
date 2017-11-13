370e82c663b4dd4959aa935fff9ce52af59ab86d
Frank Jaider Rodriguez Barreto <fraqjaroba9206@gmail.com>
2017-11-13 18:21:00 -0500

    Creación de servicio web

---
+++ SAPAutenticacion/src/main/java/co/edu/javeriana/modval/servicio/sap/WSAutenticacion_BindingImpl.java
@@ -0,0 +1,15 @@
+/**
+ * WSAutenticacion_BindingImpl.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package co.edu.javeriana.modval.servicio.sap;
+
+public class WSAutenticacion_BindingImpl implements co.edu.javeriana.modval.servicio.sap.WSAutenticacion_PortType{
+    public boolean autenticar(java.lang.String usuario, java.lang.String clave) throws java.rmi.RemoteException {
+        return false;
+    }
+
+}
---
+++ SAPAutenticacion/src/main/java/co/edu/javeriana/modval/servicio/sap/WSAutenticacion_Service.java
@@ -0,0 +1,20 @@
+/**
+ * WSAutenticacion_Service.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package co.edu.javeriana.modval.servicio.sap;
+
+public interface WSAutenticacion_Service extends javax.xml.rpc.Service {
+
+/**
+ * WSDL File for WSAutenticacion
+ */
+    public java.lang.String getWSAutenticacion_PortAddress();
+
+    public co.edu.javeriana.modval.servicio.sap.WSAutenticacion_PortType getWSAutenticacion_Port() throws javax.xml.rpc.ServiceException;
+
+    public co.edu.javeriana.modval.servicio.sap.WSAutenticacion_PortType getWSAutenticacion_Port(java.net.URL portAddress) throws javax.xml.rpc.ServiceException;
+}
