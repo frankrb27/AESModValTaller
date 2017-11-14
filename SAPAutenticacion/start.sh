2d8beb53911c25bc2fe86bd3d70b8f786d8758eb
Frank <frank@frank-VirtualBox>
2017-11-13 19:44:47 -0500

    Ajuste

--- SAPAutenticacion/start.sh
+++ SAPAutenticacion/start.sh
@@ -1,3 +1,3 @@
 mvn clean package
 docker build -t sap-autenticacion .
-docker run -d -p 7070:8080 --name sap-autenticacion sap-autenticacion
+docker run -d -p 9030:8080 --name sap-autenticacion sap-autenticacion
