8bad48c989c7e32532ee0aa3566ec07301653544
frankrb27 <32852903+frankrb27@users.noreply.github.com>
2017-11-16 13:36:12 -0500

    Update chmod.sh

--- chmod.sh
+++ chmod.sh
@@ -1,9 +1,11 @@
 chmod +x AdministrarConvenios/start.sh
 chmod +x AdministrarFactura/start.sh
 chmod +x AdministrarCompensacion/start.sh
+chmod +x Utilitarios/start.sh
 chmod +x SAPAutenticacion/start.sh
 
 chmod +x AdministrarConvenios/stop.sh
 chmod +x AdministrarFactura/stop.sh
 chmod +x AdministrarCompensacion/stop.sh
-chmod +x SAPAutenticacion/stop.sh
+chmod +x Utilitarios/stop.sh
+chmod +x SAPAuthorization/stop.sh
