38b2aae709c401ebd323ddda458da8f8148db8b0
Frank Rodriguez <fraqjaroba9206@gmail.com>
2017-11-16 09:46:42 -0500

    Se elimina proyecto fallido

--- SAPAutenticacion/start.sh
+++
@@ -1,3 +0,0 @@
-mvn clean package
-docker build -t sap-autenticacion .
-docker run -d -p 9030:8080 --name sap-autenticacion sap-autenticacion
--- SAPAutenticacion/stop.sh
+++
@@ -1,3 +0,0 @@
-docker stop sap-autenticacion
-docker rm sap-autenticacion
-docker rmi sap-autenticacion
