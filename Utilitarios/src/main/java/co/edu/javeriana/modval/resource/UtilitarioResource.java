b257a797d2df9494181e8f14aac24ff803dfd242
Frank <frank@frank-VirtualBox>
2017-11-15 23:23:23 -0500

    Cambio de POST a GET

--- Utilitarios/src/main/java/co/edu/javeriana/modval/resource/UtilitarioResource.java
+++ Utilitarios/src/main/java/co/edu/javeriana/modval/resource/UtilitarioResource.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("banco/convenio/v1")
 public class UtilitarioResource {
 
-	@RequestMapping(path = "utilitario/{idFactura}/{mensaje}", method = RequestMethod.POST,
+	@RequestMapping(path = "utilitario/{idFactura}/{mensaje}", method = RequestMethod.GET,
 			produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
 	public String generarNotifiacion(@PathVariable("idFactura") String idFactura, @PathVariable("mensaje") String mensaje) {
 		System.out.println(String.format("Mensaje enviado: [idFactura: %s, mensaje: %s]", idFactura, mensaje));
